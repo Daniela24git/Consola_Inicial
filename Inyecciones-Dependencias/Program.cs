1f780125c42964042c4ec3c6ec5f64c4741025fc
Daniela24git <danielatorresdanixx2014@hotmail.com>
2021-07-21 16:40:56 -0500

    InyeccionDependencias

---
+++ Inyecciones-Dependencias/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassLibrary1;
+using Interface;
+using Personaje;
+using Ninject;
+using DoAction;
+using System.Reflection;
+namespace Inyecciones_Dependencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un nombre");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Ingresa una habilidad");
+            var skill = Console.ReadLine();
+
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            IArma nuevaArma = kernel.Get<IArma>();
+
+            PersonajeNew personaje = new PersonajeNew(name, skill);
+            Usuario user = new Usuario(nuevaArma);
+            user.Jugar(personaje);
+        }
+    }
+}
